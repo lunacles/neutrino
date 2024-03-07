794baf05033dbcc61cdcd41998a04d8a690c3274
lunacles <lunahackley@gmail.com>
2024-03-07 17:04:41 -0600

    Create log.ts

---
+++ src/utilities/log.ts
@@ -0,0 +1,17 @@
+const Log = {
+  startTime: Date.now(),
+  time(): string {
+    return `[${new Date().toISOString()}] [${((Date.now() - Log.startTime) * 0.001).toFixed(3)}]`
+  },
+  error(reason: string, err?: Error): void {
+    console.error(Log.time(), 'ERROR:', reason, err)
+  },
+  warn(reason: string): void {
+    console.warn(Log.time(), 'WARN:', reason)
+  },
+  info(info: string): void {
+    console.info(Log.time(), 'INFO:', info)
+  },
+}
+
+export default Log

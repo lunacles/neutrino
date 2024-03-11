8ec3b3a36cfc4590bfbdaaf3a8e7159710500cf7
lunacles <lunahackley@gmail.com>
2024-03-10 20:58:57 -0500

    Create removepins.ts

---
+++ src/commands/removepins.ts
@@ -0,0 +1,58 @@
+import {
+  ChatInputCommandInteraction,
+  CacheType,
+  SlashCommandBuilder,
+  SlashCommandIntegerOption,
+  SlashCommandChannelOption,
+  SlashCommandUserOption,
+  TextChannel,
+  User,
+  Collection,
+  Message,
+} from 'discord.js'
+import CommandInterface from './default.js'
+
+const RemovePins: CommandInterface = {
+  name: 'removepins',
+  description: 'Removes all or a set amount of pins from a channel.',
+  data: new SlashCommandBuilder()
+    .addChannelOption((option: SlashCommandChannelOption ): SlashCommandChannelOption => option
+      .setName('target')
+      .setDescription('The channel to remove the pins from.')
+      .setRequired(true)
+    )
+    .addIntegerOption((option: SlashCommandIntegerOption): SlashCommandIntegerOption => option
+      .setName('amount')
+      .setDescription('The amount of pins to remove. Removes all if left empty.')
+      .setMaxValue(50)
+      .setMinValue(1)
+    )
+    .addUserOption((option: SlashCommandUserOption): SlashCommandUserOption => option
+      .setName('user')
+      .setDescription('Users to remove the pins of. Non-discriminatory if left empty.')
+    ),
+
+  async execute(interaction: ChatInputCommandInteraction<CacheType>): Promise<void> {
+    const targetChannel: TextChannel = interaction.options.getChannel('target')
+    const amount: number = interaction.options.getInteger('amount') ?? 50
+    const user: User = interaction.options.getUser('user')
+
+    const pinnedMessages: Collection<string, Message<true>> = await targetChannel.messages.fetchPinned()
+
+    let userAmount = pinnedMessages.filter((value: Message<true>) => value.author.id === user?.id)
+    await interaction.reply(`Unpinning ${user != undefined ? userAmount.size : pinnedMessages.size} message(s) ${user != undefined ? `by <@${user.id}> ` : '' }from <#${targetChannel.id}>...`)
+
+    let i: number = 0
+    for (let message of pinnedMessages.values()) {
+      if (user != undefined && message.author.id !== user.id) continue
+      await message.unpin('Pruned')
+      i++
+      if (i >= amount) break
+    }
+  },
+  test(): boolean {
+    return true
+  },
+}
+
+export default RemovePins

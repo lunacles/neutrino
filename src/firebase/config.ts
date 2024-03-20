dc29359cbdb6d9310534c84347e2487cd45862c2
lunacles <lunahackley@gmail.com>
2024-03-20 17:50:12 -0500

    Create config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,21 @@
+interface Config {
+  apiKey: string
+  authDomain: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+  measurementId: string
+}
+
+const config: Config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
+
+export default config

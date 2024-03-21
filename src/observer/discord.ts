31e51a93d38d10cdd71bd56f3c67f2d520060972
lunacles <lunahackley@gmail.com>
2024-03-21 15:34:06 -0500

    Create discord.ts

---
+++ src/observer/discord.ts
@@ -0,0 +1,66 @@
+import {
+  Emoji,
+  Message,
+  Presence,
+} from 'discord.js'
+import { Timestamp } from 'firebase-admin/firestore'
+
+interface MessageLog {
+  content: string
+  timestamp: Timestamp
+  link: string
+}
+
+interface AvatarLog {
+  avatar: string
+  timestamp: Timestamp
+}
+
+interface BannerLog {
+  banner: string
+  timestamp: Timestamp
+}
+
+interface NicknameLog {
+  name: string
+  timestamp: Timestamp
+}
+
+interface ServerData {
+  nickname: string
+  avatar: string
+  joined: number,
+  avatarLog: Array<AvatarLog>
+  messageLog: Map<string, MessageLog>
+  nicknameLog: Array<NicknameLog>
+}
+
+interface PresenceLog {
+  presence: Presence
+  timestamp: Timestamp
+  guildsSeenIn: Array<string>
+}
+
+interface StringLog {
+  value: string
+  timestamp: Timestamp
+}
+
+interface UserLogInterface {
+  id: string
+  creationDate: Timestamp
+  presense: Presence  // presenceUpdate
+  presenceLogs: Array<PresenceLog> // presenceUpdate
+  servers: Map<string, ServerData> //
+  username: string  // userUpdate
+  usernameLog: Array<StringLog> // userUpdate
+  displayName: string // userUpdate
+  displayNameLog: Array<StringLog> // userUpdate
+  avatar: string // userUpdate
+  avatarLog: Array<AvatarLog> // userUpdate
+  avatarDecoration: string // userUpdate
+  banner: string // userUpdate
+  bannerLog: Array<BannerLog> // userUpdate
+}
+
+export default UserLogInterface

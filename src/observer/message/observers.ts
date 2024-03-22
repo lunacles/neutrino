5ec3066485b772baa71c849d2f5d0158905a5208
lunacles <lunahackley@gmail.com>
2024-03-22 18:09:16 -0500

    Literally nothing

--- src/observer/message/observers.ts
+++ src/observer/message/observers.ts
@@ -1,17 +1,11 @@
 import Observer from '../interface.js'
 
 import MessageCreate from './messageCreate.js'
-import MessageDelete from './messageDelete.js'
-import MessageUpdate from './messageUpdate.js'
 
 interface MessageObserver {
   create: Observer,
-  delete: Observer,
-  update: Observer,
 }
 
 const MessageObserver: MessageObserver = {
   create: MessageCreate,
-  delete: MessageDelete,
-  update: MessageUpdate,
 }

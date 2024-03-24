48b2be9ea2d695b5e8ba3eacaf63536dfb39f1ff
lunacles <lunahackley@gmail.com>
2024-03-23 21:20:35 -0500

    Rename discord.ts to datainterface.ts

--- src/observer/datainterface.ts
+++ src/observer/datainterface.ts
@@ -1,66 +1,52 @@
 import {
-
   Presence,
 } from 'discord.js'
 import { Timestamp } from 'firebase-admin/firestore'
 
-interface MessageLog {
+export interface MessageLog {
   content: string
   attachments: Array<string>
   timestamp: Timestamp
   link: string
 }
 
-interface AvatarLog {
+export interface AvatarLog {
   avatar: string
   timestamp: Timestamp
 }
 
-interface BannerLog {
+export interface BannerLog {
   banner: string
   timestamp: Timestamp
 }
 
-interface NicknameLog {
+export interface NameLog {
   name: string
   timestamp: Timestamp
 }
 
-interface ServerData {
+export interface ServerData {
   nickname: string
   avatar: string
   joined: number,
   avatarLog: Array<AvatarLog>
   messageLog: Map<string, MessageLog>
-  nicknameLog: Array<NicknameLog>
+  nicknameLog: Array<NameLog>
 }
 
-interface PresenceLog {
+export interface PresenceLog {
   presence: Presence
   timestamp: Timestamp
   guildsSeenIn: Array<string>
 }
 
-interface StringLog {
-  value: string
-  timestamp: Timestamp
-}
-
-interface UserLogInterface {
+export interface UserInfo {
   id: string
   creationDate: Timestamp
   presence: Presence  // presenceUpdate
-  presenceLogs: Array<PresenceLog> // presenceUpdate
-  servers: Map<string, ServerData> //
   username: string  // userUpdate
-  usernameLog: Array<StringLog> // userUpdate
   displayName: string // userUpdate
-  displayNameLog: Array<StringLog> // userUpdate
   avatar: string // userUpdate
-  avatarLog: Array<AvatarLog> // userUpdate
   avatarDecoration: string // userUpdate
   banner: string // userUpdate
-  bannerLog: Array<BannerLog> // userUpdate
 }
-
-export default UserLogInterface

864c792588305095c201d2ad3fa3c5c8d3620935
lunacles <lunahackley@gmail.com>
2024-03-23 21:20:49 -0500

    Restructure the code

--- src/observer/message/messageCreate.ts
+++ src/observer/message/messageCreate.ts
@@ -16,10 +16,21 @@ import {
   database
 } from '../../firebase/database.js'
 import storage from '../../firebase/storage.js'
-import UserLogInterface from '../discord.js'
+import {
+  MessageLog,
+  AvatarLog,
+  BannerLog,
+  NameLog,
+  PresenceLog,
+  UserInfo,
+} from '../datainterface.js'
+
 import Log from '../../utilities/log.js'
+import global from '../../global.js'
 
 const parseData = (data: any): any => {
+  // TODO: Fix bug that causes stack overflow on message replies
+
   // direct return for non-object/non-array data types
   if (typeof data !== 'object' || data === null) return data
   // special handling for Timestamps
@@ -29,98 +40,141 @@ const parseData = (data: any): any => {
   // handling for Maps and Objects
   let entries: Array<[string, unknown]> = data instanceof Map ? Array.from(data.entries()) : Object.entries(data)
   let result: object = Object.fromEntries(entries.map(([key, value]) => [key, parseData(value)]))
-
   return result
+}
+
+interface TaskInterface {
+  author: User
+  guildAuthor: GuildMember
+  message: Message
+  userDoc: DocumentSnapshot | DocumentReference
+  attachmentURLs: Array<string>
+
+  createUserDoc: () => Promise<void>
+  logMessage: () => Promise<void>
+}
+
+const Task: TaskInterface = {
+  author: null,
+  guildAuthor: null,
+  message: null,
+  userDoc: null,
+  attachmentURLs: [],
+  async createUserDoc(): Promise<void> {
+    // main doc & user info
+    await database.mkdir(this.author.id, parseData({
+      id: this.author.id,
+      creationDate: Timestamp.fromDate(this.author.createdAt),
+      username: this.author.username,
+      presence: this.guildAuthor.presence,
+      displayName: this.author.displayName,
+      avatar: this.author.avatarURL(),
+      banner: this.author.bannerURL(),
+      avatarDecoration: this.author.avatarDecorationURL(),
+    } as UserInfo))
+
+    // logs collection
+    database.cd(`${this.author.id}/logs`)
+
+    await database.mkdir('presence', parseData(global.loggingConfig.presence ? {
+      log: [{
+        presence: this.guildAuthor.presence,
+        guildsSeenIn: [this.message.guild.id],
+        timestamp: Timestamp.now()
+      } as PresenceLog]
+    } : {}))
+    await database.mkdir('usernames', parseData(global.loggingConfig.usernames ? {
+      log: [{
+        name: this.author.username,
+        timestamp: Timestamp.now(),
+      } as NameLog]
+    } : {}))
+    await database.mkdir('displayNames', parseData(global.loggingConfig.displayNames ? {
+      log: [{
+      name: this.author.displayName,
+      timestamp: Timestamp.now(),
+    } as NameLog]
+   } : {}))
+    await database.mkdir('avatars', parseData(global.loggingConfig.avatars ? {
+      log: [{
+        avatar: this.author.avatarURL(),
+        timestamp: Timestamp.now(),
+      } as AvatarLog]
+    } : {}))
+    await database.mkdir('banners', parseData(global.loggingConfig.banners ? {
+      log: [{
+        banner: this.author.bannerURL() ? this.author.bannerURL() : null,
+        timestamp: Timestamp.now(),
+      } as BannerLog]
+    } : {}))
+
+    // server logs collection
+    await database.mkdir('servers', parseData(global.loggingConfig.servers ? {
+      avatars: global.loggingConfig.avatars ? [{
+        avatar: this.guildAuthor.avatarURL(),
+        timestamp: Timestamp.now(),
+      } as AvatarLog] : [],
+      messages: global.loggingConfig.messages ? [{
+        content: this.message.content,
+        attachments: this.attachmentURLs,
+        timestamp: Timestamp.fromMillis(this.message.createdTimestamp),
+        link: this.message.url,
+      } as MessageLog] : [],
+      nicknames: global.loggingConfig.nicknames ? [{
+        name: this.guildAuthor.nickname,
+        timestamp: Timestamp.now()
+      } as NameLog] : {}
+    } : {}))
+  },
+  async logMessage(): Promise<void> {
+    // path to the user's storage file
+    storage.cd(`~/${this.author.id}/servers/${this.message.guild.id}/messages/${this.message.id}`)
+
+    // add the message to their userLog
+    if (global.loggingConfig.messages) {
+      await database.write({
+        [`servers.${this.message.guild.id}.messageLog`]: FieldValue.arrayUnion(parseData({
+          content: this.message.content,
+          attachments: this.attachmentURLs,
+          timestamp: Timestamp.fromMillis(this.message.createdTimestamp),
+          link: this.message.url,
+        }))
+      })
+      Log.info(`Logging user ${this.author.id}'s message in guild ${this.message.guild.id}`)
+    }
+  }
 }
 
 const MessageCreate: Observer = {
   eventID: Events.MessageCreate,
   active: false,
   async react(bot: Client, message: Message): Promise<void> {
-    const guildAuthor: GuildMember = await message.guild.members.fetch(message.author.id)
-    const author: User = await bot.users.fetch(message.author.id, { force: true })
-    // if the user is a bot, ignore them
-    if (author.bot) return
+    Task.guildAuthor = await message.guild.members.fetch(message.author.id)
+    Task.author = await bot.users.fetch(message.author.id, { force: true })
+    Task.message = message
 
-    // path to the user's file
-    storage.cd('~').cd(`${author.id}/servers/${message.guild.id}/messages/${message.id}`)
-    let attachmentURLs: Array<string> = []
+    // collect attachments
     for (let attachment of message.attachments.values())
-      attachmentURLs.push(await storage.upload(attachment.url))
+      Task.attachmentURLs.push(await storage.upload(attachment.url))
 
+    // collect linked attachments
     for (let embed of message.embeds.values()) {
       if (!embed.data.url.includes('tenor.com/view/') && !embed.data.url.includes('cdn.discordapp.com')) continue
       if (embed.data.video)
-        attachmentURLs.push(await storage.upload(embed.data.video.url))
+        Task.attachmentURLs.push(await storage.upload(embed.data.video.url))
       if (embed.data.image)
-        attachmentURLs.push(await storage.upload(embed.data.image.url))
+        Task.attachmentURLs.push(await storage.upload(embed.data.image.url))
     }
-    database.pathToCollection('users')
-    let userDoc: DocumentSnapshot | DocumentReference = await database.pathToDoc(author.id).get()
 
-    if (!userDoc.exists) {
-      let doc: UserLogInterface = {
-        id: author.id,
-        creationDate: Timestamp.fromDate(author.createdAt),
-        presence: guildAuthor.presence,
-        presenceLogs: [{
-          presence: guildAuthor.presence,
-          guildsSeenIn: [message.guild.id],
-          timestamp: Timestamp.now()
-        }],
-        servers: new Map().set(message.guild.id, {
-          nickname: guildAuthor.nickname,
-          avatar: guildAuthor.avatarURL(),
-          joined: guildAuthor.joinedTimestamp,
-          avatarLog: [{
-            avatar: guildAuthor.avatarURL(),
-            timestamp: Timestamp.now(),
-          }],
-          messageLog: [{
-            content: message.content,
-            attachments: attachmentURLs,
-            timestamp: Timestamp.fromMillis(message.createdTimestamp),
-            link: message.url,
-          }],
-          nicknameLog: [{
-            name: guildAuthor.nickname,
-            timestamp: Timestamp.now()
-          }]
-        }),
-        username: author.username,
-        usernameLog: [{
-          value: author.username,
-          timestamp: Timestamp.now(),
-        }],
-        displayName: author.displayName,
-        displayNameLog: [{
-          value: author.displayName,
-          timestamp: Timestamp.now(),
-        }],
-        avatar: author.avatarURL(),
-        avatarLog: [{
-          avatar: author.avatarURL(),
-          timestamp: Timestamp.now(),
-        }],
-        banner: author.bannerURL() ? author.bannerURL() : null,
-        bannerLog: [{
-          banner: author.bannerURL() ? author.bannerURL() : null,
-          timestamp: Timestamp.now(),
-        }],
-        avatarDecoration: author.avatarDecorationURL()
-      }
-      userDoc = await (await database.createDoc(message.author.id, parseData(doc))).get()
-    } else {
-      await database.write({
-        [`servers.${message.guild.id}.messageLog`]: FieldValue.arrayUnion(parseData({
-          content: message.content,
-          attachments: attachmentURLs,
-          timestamp: Timestamp.fromMillis(message.createdTimestamp),
-          link: message.url,
-        }))
-      })
-      Log.info(`Logging user ${author.id}'s message in guild ${message.guild.id}`)
-    }
+    // if the user is a bot, ignore them
+    if (Task.author.bot) return
+
+    // path to the users document
+    database.cd('~')
+
+    // if they don't already exist in the database, add them
+    if (await database.cat(Task.author.id) == null)
+      await Task.createUserDoc()
   }
 }
 

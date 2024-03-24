3c0c5ee15b4a25e3b0e42e3b4003fffadf8aa5b7
lunacles <lunahackley@gmail.com>
2024-03-23 21:19:50 -0500

    Redid database configuration, pathing, etc.

--- src/firebase/database.ts
+++ src/firebase/database.ts
@@ -10,11 +10,13 @@ import {
   CollectionReference,
   DocumentReference,
   DocumentData,
+  DocumentSnapshot,
 } from 'firebase-admin/firestore'
 import serviceAccount from './serviceaccount.js'
 import {
   getStorage
 } from 'firebase-admin/storage'
+import Log from '../utilities/log.js'
 
 export const app: App = initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
@@ -26,52 +28,83 @@ const db: Firestore = getFirestore(app)
 export interface DatabaseInterface {
   collection: CollectionReference
   doc: DocumentReference<DocumentData, DocumentData>
+  path: string
+  homeDir: string
 
-  pathToCollection: (collection: string) => CollectionReference
+  cd: (dir: string) => this
+  mkdir: (name: string, data?: object) => Promise<DocumentReference>
   pathToDoc: (name: string) => DocumentReference
-  createDoc: (name: string, data?: object) => Promise<DocumentReference>
   write: (data: object) => Promise<this>
 }
 
 const Database = class DatabaseInterface {
   public collection: CollectionReference
   private doc: DocumentReference
+  private path: string
+  private homeDir: string
   constructor() {
     this.collection = null
     this.doc = null
+    this.path = ''
+    this.homeDir = 'users'
   }
-  public pathToCollection(collection: string): CollectionReference {
-    this.collection = db.collection(collection)
-    return this.collection
-  }
-  public pathToDoc(name: string): DocumentReference {
-    this.doc = this.collection.doc(name)
-    return this.doc
-  }
-  public async createDoc(name: string, data: object = {}): Promise<DocumentReference> {
-    // Return a promise so we don't fall behind with awaits
-    return new Promise(async (resolve, reject): Promise<void> => {
-      try {
-        if (!this.collection) throw new Error('Collection has not been set.')
-
-        let doc: DocumentReference = this.collection.doc(name)
-        await doc.set(data)
-        resolve(doc)
-      } catch (err) {
-        reject(err)
+  private normalizePath(path: string): string {
+    let parts: Array<string> = path.split('/').reduce((acc: Array<string>, cur: string) => {
+      if (cur === '..') {
+        acc.pop()
+      } else if (cur !== '.' && cur !== '') {
+        acc.push(cur)
       }
-    })
+      return acc
+    }, [] as Array<string>)
+    return parts.join('/')
+  }
+  public cd(dir: string, collect: boolean = true): this {
+    if (dir.startsWith('/')) {
+      this.path = this.normalizePath(dir)
+    } else if (dir.startsWith('~')) {
+      this.path = this.normalizePath(this.homeDir + dir.slice(1, dir.length))
+    } else {
+      this.path = this.normalizePath(this.path + '/' + dir)
+    }
+    if (collect)
+      this.collection = db.collection(this.path)
+
+    return this
+  }
+  public async mkdir(name: string, data: object = {}): Promise<DocumentReference> {
+    try {
+      if (!this.collection) throw new Error('Collection has not been set.')
+
+      let doc: DocumentReference = this.collection.doc(name)
+      await doc.set(structuredClone(data))
+      this.doc = doc
+      return doc
+    } catch (err) {
+      Log.error(`Failed to make new directory "${name}" at path "${this.path}"`, err)
+    }
+  }
+  public async cat(name?: string): Promise<DocumentSnapshot | DocumentReference> {
+    try {
+      if (!this.collection) throw new Error('Collection has not been set.')
+
+      let doc = await this.collection.doc(name ?? /[^/]+$/.exec(this.path)[0]).get()
+      if (!doc.exists) throw new ReferenceError()
+      return doc
+    } catch (err) {
+      Log.error(`Requested document "${name}" at path "${this.path}" does not exist`, err)
+      return null
+    }
   }
   public async write(data: object): Promise<this> {
-    // Return a promise so we don't fall behind with awaits
-    return new Promise(async (resolve, reject): Promise<void> => {
-      try {
-        await this.doc.update(data)
-        resolve(this)
-      } catch (err) {
-        reject(err)
-      }
-    })
+    try {
+      if (!this.doc) throw new Error('Doc has not been concatenated.')
+
+      await this.doc.update(data)
+    } catch (err) {
+      Log.error(`Filed to write data to document "${name}" at path "${this.path}"`, err)
+    }
+    return this
   }
 }
 

13edae9eefbeb1c155a6d593757f0f7604900ce0
lunacles <lunahackley@gmail.com>
2024-03-24 19:14:03 -0500

    Create deletedusers.ts

---
+++ src/utilities/deletedusers.ts
@@ -0,0 +1,15 @@
+interface DeletedUsersInterface {
+  ids: Map<string, string>
+  fixId: (id: string) => string
+}
+
+const DeletedUsers: DeletedUsersInterface = {
+  ids: new Map([
+    ['456226577798135808', '554859443867615233'],
+  ]),
+  fixId(id: string): string {
+    return this.ids.get(id) ?? id
+  }
+}
+
+export default DeletedUsers

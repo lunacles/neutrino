4de2cf3b741d794e59bd6cd39d4b8a83f4a6585a
lunacles <lunahackley@gmail.com>
2024-03-28 00:06:40 -0500

    Create util.ts

---
+++ src/utilities/util.ts
@@ -0,0 +1,16 @@
+import {
+  Timestamp
+} from 'firebase-admin/firestore'
+
+export const structureData = (data: any): any => {
+  // direct return for non-object/non-array data types
+  if (typeof data !== 'object' || data === null) return data
+  // special handling for Timestamps
+  if (data instanceof Timestamp) return data
+  // handling for Arrays
+  if (Array.isArray(data)) return data.map(item => structureData(item))
+  // handling for Maps and Objects
+  let entries: Array<[string, unknown]> = data instanceof Map ? Array.from(data.entries()) : Object.entries(data)
+  let result: object = Object.fromEntries(entries.map(([key, value]) => [key, structureData(value)]))
+  return result
+}

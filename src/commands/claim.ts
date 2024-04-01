74d3360d45efb85233b4296423d6bb0ab07055f0
lunacles <lunahackley@gmail.com>
2024-03-31 19:58:15 -0500

    Buff claim amount & change reply to embed

--- src/commands/claim.ts
+++ src/commands/claim.ts
@@ -2,6 +2,7 @@ import {
   ChatInputCommandInteraction,
   CacheType,
   SlashCommandBuilder,
+  EmbedBuilder,
 } from 'discord.js'
 import CommandInterface from './interface.js'
 import InteractionObserver from './interactionobserver.js'
@@ -11,6 +12,7 @@ import {
 } from '../firebase/database.js'
 import * as util from '../utilities/util.js'
 import UserData from '../firebase/userdoc.js'
+import Icon from '../utilities/icon.js'
 
 const Claim: CommandInterface = {
   name: 'claim',
@@ -19,7 +21,6 @@ const Claim: CommandInterface = {
   async execute(interaction: ChatInputCommandInteraction<CacheType>): Promise<void> {
     await interaction.deferReply()
     const observer = new InteractionObserver(interaction)
-
     if (interaction.guild.id !== global.arrasDiscordId) return await observer.abort(3)
 
     let user = Database.users.get(interaction.user.id) ?? await UserData.new(interaction.user.id, interaction.guild)
@@ -29,12 +30,39 @@ const Claim: CommandInterface = {
       interaction.editReply(`This command is on cooldown for **${util.formatSeconds(global.cooldown.claim - cooldown, true)}!**`)
       return
     } else {
-      // Get a random score between 10 and 100
-      let claimedScore: number = Math.floor(Math.random() * (100 - 10) + 10)
-
+      // Get a random score between 1 and 100
+      let claimedScore: number = Math.floor(Math.random() * (200 - 15) + 15)
+      let icon: string
+      if (claimedScore < 40) {
+        icon = Icon.ReceiveMoney
+      } else if (claimedScore < 80) {
+        icon = Icon.Cash
+      } else if (claimedScore < 120) {
+        icon = Icon.Coins
+      } else if (claimedScore < 160) {
+        icon = Icon.GoldBar
+      } else {
+        icon = Icon.OpenTreasureChest
+      }
       await user.misc.scoreGame.setScore(interaction.guild, data.score + claimedScore)
-      interaction.editReply(`You have claimed **${claimedScore.toLocaleString()} points!**`)
       await user.misc.scoreGame.setCooldown(interaction.guild, 'claim', Date.now())
+
+      const embed = new EmbedBuilder()
+        .setColor(user.author.hexAccentColor)
+        .setAuthor({
+          name: `${user.author.username}`,
+          iconURL: user.author.avatarURL(),
+        })
+        .setThumbnail(`attachment://${icon}`)
+        .setDescription(`# You have claimed **${claimedScore.toLocaleString()} points!**`)
+
+      interaction.editReply({
+        embeds: [embed],
+        files: [{
+          attachment: `./src/utilities/assets/${icon}`,
+          name: icon,
+        }]
+      })
     }
   },
   test(): boolean {

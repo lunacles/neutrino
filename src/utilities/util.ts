c06aabc3b9e65b5464d3d6eaeeec35d8e75b7b15
lunacles <lunahackley@gmail.com>
2024-04-05 16:48:25 -0500

    Add dateSuffix & formatDate functions

--- src/utilities/util.ts
+++ src/utilities/util.ts
@@ -1,6 +1,7 @@
 import {
   Timestamp
 } from 'firebase-admin/firestore'
+import Log from './log.js'
 
 export const structureData = (data: any): any => {
   // direct return for non-object/non-array data types
@@ -36,3 +37,28 @@ export const formatSeconds = (seconds: number, s: boolean = false): string => {
 export const averageArray = (array: Array<number>): number => array.length ? array.reduce((a: number, b: number) => a + b) / array.length : 0
 
 export const sumArray = (array: Array<number>): number => array.reduce((a: number, b: number) => a + b, 0)
+
+export const dateSuffix = (day: number): string => {
+  let last: number = day % 10
+  let lastTwo: number = day % 100
+  if ([11, 12, 13].includes(lastTwo)) return 'th'
+  switch (last) {
+    case 1: return 'st'
+    case 2: return 'nd'
+    case 3: return 'rd'
+    default: return 'th'
+  }
+}
+
+export const formatDate = (string: string): string => {
+  let date: Date = new Date(string)
+  try {
+    let months: Array<string> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    if (!(date instanceof Date && !isNaN(date.getDate()))) throw new Error('Invalid date')
+    let day: number = date.getUTCDate()
+    let name: string = months[date.getUTCMonth()]
+    return `${name} ${day}${dateSuffix(day)}, ${date.getUTCFullYear()}`
+  } catch (err) {
+    Log.error('Failed to retrieve date', err)
+  }
+}

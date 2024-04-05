276470897546118e258898d92550e387d85e2508
lunacles <lunahackley@gmail.com>
2024-04-05 15:54:00 -0500

    Create interface, make uptime getter, make time method getter

--- src/utilities/log.ts
+++ src/utilities/log.ts
@@ -1,16 +1,30 @@
-const Log = {
+import * as util from './util.js'
+
+interface LogInterface {
+  startTime: number
+  uptime: string
+  time: string
+  error(reason: string, err?: Error | object): void
+  warn(reason: string): void
+  info(info: string): void
+}
+
+const Log: LogInterface = {
   startTime: Date.now(),
-  time(): string {
+  get uptime(): string {
+    return util.formatSeconds(process.uptime())
+  },
+  get time(): string {
     return `[${new Date().toISOString()}] [${((Date.now() - Log.startTime) * 0.001).toFixed(3)}]`
   },
   error(reason: string, err?: Error | object): void {
-    console.error(Log.time(), 'ERROR:', reason, err)
+    console.error(Log.time, 'ERROR:', reason, err)
   },
   warn(reason: string): void {
-    console.warn(Log.time(), 'WARN:', reason)
+    console.warn(Log.time, 'WARN:', reason)
   },
   info(info: string): void {
-    console.info(Log.time(), 'INFO:', info)
+    console.info(Log.time, 'INFO:', info)
   },
 }
 

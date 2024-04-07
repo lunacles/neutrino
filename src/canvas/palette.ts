b5bee8b9bd598f80ce568ecbc437ed39c783ab57
lunacles <lunahackley@gmail.com>
2024-04-06 23:44:56 -0500

    Create palette.ts

---
+++ src/canvas/palette.ts
@@ -0,0 +1,15 @@
+import Color from './color.js'
+
+const Colors = {
+  pureBlack: new Color('#000000'),
+  pureWhite: new Color('#ffffff'),
+  white: new Color('#f6f6f6'),
+  black: new Color('#0e0e0e'),
+
+  darkBlue: new Color('#081322'),
+  glacialBlue: new Color('#afe1fb'),
+  cyan: new Color('#6191ae'),
+  pastelBlue: new Color('#8ec6e6'),
+}
+
+export default Colors

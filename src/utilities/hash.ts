7ca2f5318cf673ce69b6e0585a264c66198f80be
lunacles <lunahackley@gmail.com>
2024-04-10 23:23:49 -0500

    Create hash.ts

---
+++ src/utilities/hash.ts
@@ -0,0 +1,22 @@
+export interface HashInterface {
+  cyrb53: (str: string, seed?: number) => number
+}
+
+export const Hash: HashInterface = {
+  // 53-bit string hash - https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+  cyrb53(str: string = '', seed: number = 0): number {
+    let h1 = 0xdeadbeef ^ seed
+    let h2 = 0x41c6ce57 ^ seed
+    let ch: number
+    for (let i = 0; i < str.length; i++) {
+      ch = str.charCodeAt(i)
+      h1 = Math.imul(h1 ^ ch, 0x85ebca77)
+      h2 = Math.imul(h2 ^ ch, 0xc2b2ae3d)
+    }
+    h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97)
+    h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9)
+    h1 ^= h2 >>> 16
+    h2 ^= h1 >>> 16
+    return 2097152 * (h2 >>> 0) + (h1 >>> 11)
+  }
+}

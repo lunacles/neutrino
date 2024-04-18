dc102d41ebb96c46a61a0d118c1fadfe2cad9902
lunacles <lunahackley@gmail.com>
2024-04-18 14:52:45 -0500

    Add options to have more control over multiScale and domainWarp

--- src/mazes/algorithms/noise.ts
+++ src/mazes/algorithms/noise.ts
@@ -41,13 +41,18 @@ export interface NoiseInterface {
 enum DefaultSettings {
   Zoom = 1,
   Threshold = 0.1,
-  Min = -0.085,
-  Max = 0.085,
+  Min = -0.3,
+  Max = 0.3,
   Iterations = 1,
   Power = 5,
   Size = 16,
   X = 5,
   Y = 5,
+  Amplitude = 1,
+  Frequency = 1,
+  AmplitudeMultiplier = 0.5,
+  FrequencyMultiplier = 2,
+  Warp = 50,
 }
 
 export const Noise = class NoiseInterface {
@@ -62,6 +67,11 @@ export const Noise = class NoiseInterface {
   private max: number
   private turbulence: Turbulence
   private repetition: Coordinate
+  private amplitude: number
+  private frequency: number
+  private amplitudeMultiplier: number
+  private frequencyMultiplier: number
+  private warp: number
   constructor() {
     this.maze = null
     this.ran = null
@@ -79,6 +89,11 @@ export const Noise = class NoiseInterface {
       x: DefaultSettings.X,
       y: DefaultSettings.Y,
     }
+    this.amplitude = DefaultSettings.Amplitude
+    this.frequency = DefaultSettings.Frequency
+    this.amplitudeMultiplier = DefaultSettings.AmplitudeMultiplier
+    this.frequencyMultiplier = DefaultSettings.FrequencyMultiplier
+    this.warp = DefaultSettings.Warp
   }
   public init(): void {
     this.perlin = new ImprovedNoise(this.ran)
@@ -106,55 +121,75 @@ export const Noise = class NoiseInterface {
     this.type = type
     return this
   }
-  private iter(iterator: Function): void {
+  private mapBy(map: Function): void {
     for (let y: number = 0; y < this.maze.height; y++) {
       for (let x: number = 0; x < this.maze.width; x++) {
         if (!this.validateCell({ x, y })) continue
 
         let value: any
-        value = iterator(x, y)
+        value = map(x, y)
 
-        this.maze.set(x, y, +!value)
+        this.maze.set(x, y, +value)
       }
     }
   }
   public normal(): this {
-    this.iter((x: number, y: number): boolean => {
+    this.mapBy((x: number, y: number): boolean => {
       return this.perlin.noise(x / this.zoom, y / this.zoom, 0) > 0
     })
     return this
   }
   public clamped(): this {
-    this.iter((x: number, y: number): boolean => {
+    this.mapBy((x: number, y: number): boolean => {
       let noise: number = this.perlin.noise(x / this.zoom, y / this.zoom, 0)
       return noise < this.max && noise > this.min
     })
     return this
   }
   public quantized(): this {
-    this.iter((x: number, y: number): number => {
+    this.mapBy((x: number, y: number): number => {
       let noise: number = this.perlin.noise(x / this.zoom, y / this.zoom, 0)
       return this.perlin.quantize(noise, this.threshold)
     })
     return this
   }
+  public setWarp(warp: number): this {
+    this.warp = warp
+    return this
+  }
   public domainWarped(): this {
-    this.iter((x: number, y: number): boolean => {
-      let warp = this.perlin.domainWarp(x / this.zoom, y / this.zoom, 0)
+    this.mapBy((x: number, y: number): boolean => {
+      let warp = this.perlin.domainWarp(this.warp, x / this.zoom, y / this.zoom, 0)
       return this.perlin.noise(warp.x, warp.y, 0) > 0
     })
     return this
   }
   public dynamic(): this {
-    this.iter((x: number, y: number): number => {
+    this.mapBy((x: number, y: number): number => {
       let noise: number = this.perlin.dynamic(x / this.zoom, y / this.zoom, 0, new Date((new Date()).getTime() * 0.001))
       return this.perlin.quantize(noise, this.threshold)
     })
     return this
   }
+  public setAmplitude(amplitude: number): this {
+    this.amplitude = amplitude
+    return this
+  }
+  public setFrequency(frequency: number): this {
+    this.frequency = frequency
+    return this
+  }
+  public setAmplitudeMultiplier(multiplier: number): this {
+    this.amplitudeMultiplier = multiplier
+    return this
+  }
+  public setFrequencyMultiplier(multiplier: number): this {
+    this.frequencyMultiplier = multiplier
+    return this
+  }
   public multiScale(): this {
-    this.iter((x: number, y: number): boolean => {
-      return this.perlin.multiScale(x / this.zoom, y / this.zoom, 0) > 0
+    this.mapBy((x: number, y: number): boolean => {
+      return this.perlin.multiScale(this.amplitude, this.frequency, this.amplitudeMultiplier, this.frequencyMultiplier, x / this.zoom, y / this.zoom, 0) > 0
     })
     return this
   }
@@ -186,7 +221,7 @@ export const Noise = class NoiseInterface {
     return this
   }
   public marble(): this {
-    this.iter((x: number, y: number): boolean => {
+    this.mapBy((x: number, y: number): boolean => {
       let value: number = x * this.repetition.x / this.maze.width + y * this.repetition.y / this.maze.height + this.turbulence.power * this.applyTurbulence(x, y, this.turbulence.size) / 256
       let sin: number = 256 * Math.abs(Math.sin(value * Math.PI))
 

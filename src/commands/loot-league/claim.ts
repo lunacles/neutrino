1cc3a11e3f049de2bf5da7e52adec930a37a4677
lunacles <lunahackley@gmail.com>
2024-05-28 01:33:46 -0500

    Add panic catch

--- src/commands/loot-league/claim.ts
+++ src/commands/loot-league/claim.ts
@@ -27,49 +27,52 @@ const Claim: CommandInterface = {
     //if (interaction.guild.id !== global.testServerId) return await observer.abort(3)
     if (interaction.channel.id !== global.commandChannels.lootLeague && !observer.checkPermissions([PermissionsBitField.Flags.ManageMessages], interaction.channel))
       return await observer.abort(5)
+    try {
+      let guild: GuildCollectionInterface = await GuildCollection.fetch(interaction.guildId)
+      let userData: UserDataInterface = await guild.fetchMember(interaction.user.id)
+      let lootLeague: LootLeagueInterface = userData.lootLeague
 
-    let guild: GuildCollectionInterface = await GuildCollection.fetch(interaction.guildId)
-    let userData: UserDataInterface = await guild.fetchMember(interaction.user.id)
-    let lootLeague: LootLeagueInterface = userData.lootLeague
-
-    let cooldown: number = Math.floor((Date.now() - lootLeague.cooldown.claim) / 1e3)
-    if (cooldown < global.cooldown.claim) {
-      interaction.editReply(`This command is on cooldown for **${util.formatSeconds(global.cooldown.claim - cooldown, true)}!**`)
-      return
-    } else {
-      // Get a random score between 1 and 100
-      let claimedScore: number = Math.floor(Math.random() * (200 - 15) + 15)
-      let icon: string
-      if (claimedScore < 40) {
-        icon = Icon.ReceiveMoney
-      } else if (claimedScore < 80) {
-        icon = Icon.Cash
-      } else if (claimedScore < 120) {
-        icon = Icon.Coins
-      } else if (claimedScore < 160) {
-        icon = Icon.GoldBar
+      let cooldown: number = Math.floor((Date.now() - lootLeague.cooldown.claim) / 1e3)
+      if (cooldown < global.cooldown.claim) {
+        interaction.editReply(`This command is on cooldown for **${util.formatSeconds(global.cooldown.claim - cooldown, true)}!**`)
+        return
       } else {
-        icon = Icon.OpenTreasureChest
-      }
-      await lootLeague.setScore(lootLeague.score + claimedScore)
-      await lootLeague.setCooldown('claim', Date.now())
+        // Get a random score between 1 and 100
+        let claimedScore: number = Math.floor(Math.random() * (200 - 15) + 15)
+        let icon: string
+        if (claimedScore < 40) {
+          icon = Icon.ReceiveMoney
+        } else if (claimedScore < 80) {
+          icon = Icon.Cash
+        } else if (claimedScore < 120) {
+          icon = Icon.Coins
+        } else if (claimedScore < 160) {
+          icon = Icon.GoldBar
+        } else {
+          icon = Icon.OpenTreasureChest
+        }
+        await lootLeague.setScore(lootLeague.score + claimedScore)
+        await lootLeague.setCooldown('claim', Date.now())
 
-      const embed = new EmbedBuilder()
-        .setColor(userData.user.hexAccentColor)
-        .setAuthor({
-          name: `${userData.user.username}`,
-          iconURL: userData.user.avatarURL(),
+        const embed = new EmbedBuilder()
+          .setColor(userData.user.hexAccentColor)
+          .setAuthor({
+            name: `${userData.user.username}`,
+            iconURL: userData.user.avatarURL(),
+          })
+          .setThumbnail(`attachment://${icon}`)
+          .setDescription(`# You have claimed **${claimedScore.toLocaleString()} points!**`)
+
+        interaction.editReply({
+          embeds: [embed],
+          files: [{
+            attachment: `./src/utilities/assets/${icon}`,
+            name: icon,
+          }]
         })
-        .setThumbnail(`attachment://${icon}`)
-        .setDescription(`# You have claimed **${claimedScore.toLocaleString()} points!**`)
-
-      interaction.editReply({
-        embeds: [embed],
-        files: [{
-          attachment: `./src/utilities/assets/${icon}`,
-          name: icon,
-        }]
-      })
+      }
+    } catch (err) {
+      await observer.panic(err, this.name)
     }
   },
   test(): boolean {

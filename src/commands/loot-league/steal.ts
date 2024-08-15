2ad688ebb4628cde6d4bd46a2649340449e4b0e8
lunacles <lunahackley@gmail.com>
2024-08-15 12:09:22 -0500

    Add dynamic steal chance

--- src/commands/loot-league/steal.ts
+++ src/commands/loot-league/steal.ts
@@ -5,18 +5,15 @@ import {
   SlashCommandUserOption,
   EmbedBuilder,
   PermissionsBitField,
+  User,
 } from 'discord.js'
-import {
-  CommandInterface,
-  GuildCollectionInterface,
-  UserDataInterface,
-  LootLeagueInterface,
-} from '../../types.js'
-import GuildCollection from '../../user-manager/guildcollection.js'
+//import GuildCollection from '../../user-manager/guildcollection.js'
 import InteractionObserver from '../interactionobserver.js'
-import global from '../../global.js'
+import global from 'global.js'
 import * as util from '../../utilities/util.js'
 import Icon from '../../utilities/icon.js'
+import Database from 'db/database.js'
+import { Abort } from 'types/enum.d.js'
 
 const Steal: CommandInterface = {
   name: 'steal',
@@ -28,101 +25,87 @@ const Steal: CommandInterface = {
     .setRequired(true)
   ),
   async execute(interaction: ChatInputCommandInteraction<CacheType>): Promise<void> {
-    await interaction.deferReply()
+    const observer = await new InteractionObserver(interaction).defer()
+    const user: User = await util.fetchUser(interaction.user.id)
     const targetUserOption = interaction.options.getUser('user', true)
-    const observer = new InteractionObserver(interaction)
-    //if (interaction.guild.id !== global.testServerId) return await observer.abort(3)
+
+    //if (interaction.guild.id !== global.testServerId) return await observer.abort(Abort.CommandUnavailableInServer)
     if (interaction.channel.id !== global.commandChannels.lootLeague && !observer.checkPermissions([PermissionsBitField.Flags.ManageMessages], interaction.channel))
-      return await observer.abort(5)
+      return await observer.abort(Abort.CommandRestrictedChannel)
 
-    if (targetUserOption.id === interaction.user.id) return await observer.abort(4)
+    if (targetUserOption.id === user.id) return await observer.abort(Abort.SelfTargetNotAllowed)
 
-    let guild: GuildCollectionInterface = await GuildCollection.fetch(interaction.guildId)
-    let userData: UserDataInterface = await guild.fetchMember(interaction.user.id)
-    let targetData: UserDataInterface = await guild.fetchMember(targetUserOption.id)
+    let userData: DatabaseInstanceInterface = await Database.discord.users.fetch(user.id)
+    let targetData: DatabaseInstanceInterface = await Database.discord.users.fetch(targetUserOption.id)
 
-    let userLootLeague: LootLeagueInterface = userData.lootLeague
-    let targetLootLeague: LootLeagueInterface = targetData.lootLeague
-    if (userLootLeague.shieldEnd > Date.now()) {
-      interaction.editReply('You cannot steal from someone while you have a shield active!')
+    if (userData.shieldEnd > Date.now()) {
+      await interaction.editReply('You cannot steal from someone while you have a shield active!')
       return
-    } else if (targetLootLeague.score < 250) {
-      interaction.editReply('You cannot steal from someone with less than 250 points!')
+    } else if (targetData.score < 250) {
+      await interaction.editReply('You cannot steal from someone with less than 250 points!')
       return
-    } else if (userLootLeague.score < 250) {
-      interaction.editReply('You cannot steal from someone if you have less than 250 points!')
+    } else if (targetData.score < 250) {
+      await interaction.editReply('You cannot steal from someone if you have less than 250 points!')
       return
-    } else if (targetLootLeague.shieldEnd > Date.now()) {
-      interaction.editReply(`You cannot steal from someone who has a shield active!\nTheir shield will expire <t:${Math.floor(targetLootLeague.shieldEnd / 1e3)}:R>`)
+    } else if (targetData.shieldEnd > Date.now()) {
+      await interaction.editReply(`You cannot steal from someone who has a shield active!\nTheir shield will expire <t:${Math.floor(targetData.shieldEnd / 1e3)}:R>`)
       return
     }
 
-    let cooldown: number = Math.floor((Date.now() - userLootLeague.cooldown.steal) / 1e3)
-    if (cooldown < global.cooldown.steal) {
-      interaction.editReply(`This command is on cooldown for **${util.formatSeconds(global.cooldown.steal - cooldown, true)}!**`)
+    if (observer.isOnCooldown('steal')) {
+      await interaction.editReply(`This command is on cooldown for **${util.formatSeconds(observer.getCooldown('steal'), true)}!**`)
       return
     } else {
-      let succeed: boolean = Math.random() < 0.5
-      let stolenScore: number = Math.floor(Math.max(targetLootLeague.score * 0.3 * Math.random(), targetLootLeague.score * 0.3 * 0.25))
+      const factor: number = 1.65
+      let chance: number = userData.score / targetData.score * Math.sqrt(factor)
+      // pentalize players stealing from players with a lower score than theirs
+      if (targetData.score <= userData.score)
+        chance = targetData.score / userData.score / Math.sqrt(factor)
 
-      if (succeed) {
-        await userLootLeague.setScore(userLootLeague.score + stolenScore)
-        await targetLootLeague.setScore(targetLootLeague.score - stolenScore)
-        const embed = new EmbedBuilder()
-          .setColor(userData.user.hexAccentColor)
-          .setAuthor({
-            name: `${userData.user.username}`,
-            iconURL: userData.user.avatarURL(),
-          })
-          .setThumbnail(`attachment://${Icon.Robber}`)
-          .setDescription(`# You successfully stole __${stolenScore.toLocaleString()}__ from <@${targetData.user.id}>!`)
-          .addFields({
-            name: 'Your New Points',
-            value: userLootLeague.score.toLocaleString(),
-            inline: true,
-          }, {
-            name: `${targetData.user.username}'s New Points`,
-            value: targetLootLeague.score.toLocaleString(),
-            inline: true,
-          })
+      // cap the chance at at 65%
+      let succeed: boolean = await userData.random() < Math.min(chance, 0.65)
 
-        interaction.editReply({
-          embeds: [embed],
-          files: [{
-            attachment: `./src/utilities/assets/${Icon.Robber}`,
-            name: Icon.Robber,
-          }]
+      let stolenScore: number = succeed ?
+        // steal at least 5% of target score, at most 30% of target score,
+        await userData.fromRange(targetData.score * 0.05, targetData.score * 0.3, 'Integer') :
+        // lose at least 12.5% of own score, at most 35% of own score,
+        -(await userData.fromRange(userData.score * 0.125, userData.score * 0.35, 'Integer'))
+
+      await userData.setScore(userData.score + stolenScore)
+      await targetData.setScore(targetData.score - stolenScore)
+
+      let icon = succeed ? Icon.Robber : Icon.Amputation
+
+      const embed = new EmbedBuilder()
+        .setColor(user.accentColor)
+        .setAuthor({
+          name: `${user.username}`,
+          iconURL: user.avatarURL(),
         })
-      } else {
-        await userLootLeague.setScore(Math.max(userLootLeague.score - stolenScore, 0))
-        const embed = new EmbedBuilder()
-          .setColor(userData.user.hexAccentColor)
-          .setAuthor({
-            name: `${userData.user.username}`,
-            iconURL: userData.user.avatarURL(),
-          })
-          .setThumbnail(`attachment://${Icon.Amputation}`)
-          .setDescription(`# You failed to steal from <@${targetData.user.id}> and lost __${stolenScore.toLocaleString()}__!`)
-          .addFields({
-            name: 'Your New Points',
-            value: userLootLeague.score.toLocaleString(),
-            inline: true,
-          }, {
-            name: `${targetData.user.username}'s New Points`,
-            value: targetLootLeague.score.toLocaleString(),
-            inline: true,
-          })
-
-        interaction.editReply({
-          embeds: [embed],
-          files: [{
-            attachment: `./src/utilities/assets/${Icon.Amputation}`,
-            name: Icon.Amputation,
-          }]
+        .setThumbnail(`attachment://${icon}`)
+        .setDescription(succeed ?
+          `# You successfully stole __${stolenScore.toLocaleString()}__ from <@${targetUserOption.id}>!` :
+          `# You failed to steal from <@${targetUserOption.id}> and lost __${stolenScore.toLocaleString()}__!`
+        )
+        .addFields({
+          name: 'Your New Points',
+          value: userData.score.toLocaleString(),
+          inline: true,
+        }, {
+          name: `${targetUserOption.username}'s New Points`,
+          value: targetData.score.toLocaleString(),
+          inline: true,
         })
-      }
 
-      await userLootLeague.setCooldown('steal', Date.now())
+      await interaction.editReply({
+        embeds: [embed],
+        files: [{
+          attachment: `./src/utilities/assets/${icon}`,
+          name: icon,
+        }]
+      })
+
+      observer.resetCooldown('score')
     }
   },
   test(): boolean {

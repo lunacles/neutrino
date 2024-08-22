a515c1ed8d7f47d3222944b5de57d1d556bbc15b
lunacles <lunahackley@gmail.com>
2024-08-22 00:59:52 -0500

    Move interface to types.d.ts

--- src/global.ts
+++ src/global.ts
@@ -2,31 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import Build from 'utilities/repo'
 
-interface CommandCooldownInterface {
-  readonly score: number
-  readonly claim: number
-  readonly steal: number
-  readonly gamble: number
-  readonly shield: number
-  readonly leaderboard: number
-  readonly blackjack: number
-}
-
-interface Direction {
-  none: number
-  left: number
-  right: number
-  up: number
-  down: number
-}
-
-interface DiagonalDirection {
-  readonly upLeft: number
-  readonly downLeft: number
-  readonly upRight: number
-  readonly downRight: number
-}
-
+const envVar = (key: string): string => process.env[`${process.env.NODE_ENV}_${key}`]
 const Direction: Direction = {
   none: 0,  // 0000
   left: 1,  // 0001
@@ -34,54 +10,11 @@ const Direction: Direction = {
   up: 4,    // 0100
   down: 8,  // 1000
 }
-
 const DiagonalDirections: DiagonalDirection = {
   upLeft: Direction.left | Direction.up,
   downLeft: Direction.left | Direction.down,
   upRight: Direction.right | Direction.up,
   downRight: Direction.right | Direction.down
-}
-
-interface MovementOptions {
-  all: Array<number> | number,
-  diagonal: Array<number> | number,
-  vertical: Array<number> | number,
-  horizontal: Array<number> | number,
-}
-
-interface CommandChannels {
-  readonly lootLeague: string
-  readonly mazeGeneration: string
-  readonly misc: string
-}
-
-interface ENV {
-  readonly FIREBASE_API_KEY: string
-  readonly FIREBASE_AUTH_DOMAIN: string
-  readonly FIREBASE_PROJECT_ID: string
-  readonly FIREBASE_STORAGE_BUCKET: string
-  readonly FIREBASE_MESSAGE_SENDER_ID: string
-  readonly FIREBASE_APP_ID: string
-  readonly FIREBASE_MEASUREMENT_ID: string
-  readonly BOT_TOKEN: string
-  readonly BOT_CLIENT_ID: string
-}
-
-const envVar = (key: string): string => process.env[`${process.env.NODE_ENV}_${key}`]
-
-interface GlobalInterface {
-  readonly build: any
-  readonly ownerId: string
-  readonly testServerId: string
-  readonly cooldown: CommandCooldownInterface
-  readonly shieldDuration: number
-  direction: Direction
-  diagonalDirection: DiagonalDirection
-  movementOptions: MovementOptions
-  readonly batchTick: number
-  readonly errorTraceChannel: string
-  readonly commandChannels: CommandChannels
-  readonly env: ENV
 }
 
 const global: GlobalInterface = {
@@ -95,8 +28,9 @@ const global: GlobalInterface = {
     gamble: 60,      // 1min
     shield: 60 * 60, // 1hr
     leaderboard: 30, // 30sec
-    blackjack: 60,   // 1min
+    blackjack: 30,   // 1min
   },
+  cooldowns: new Map<string, CommandCooldownInterface>(),
   shieldDuration: 60 * 30, // 30min
   direction: Direction,
   diagonalDirection: DiagonalDirections,
@@ -135,7 +69,11 @@ const global: GlobalInterface = {
     FIREBASE_MEASUREMENT_ID: envVar('FIREBASE_MEASUREMENT_ID'),
     BOT_TOKEN: envVar('BOT_TOKEN'),
     BOT_CLIENT_ID: envVar('BOT_CLIENT_ID'),
-  }
+  },
+  botId: '1195601709909692486',
+  database: null,
+  rolePersistCap: 10,
+  prefix: '!$',
 }
 
 export default global

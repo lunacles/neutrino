e1268a24b9ad80dea007287d9f5ddb1380f0a4b6
lunacles <lunahackley@gmail.com>
2024-08-31 21:49:07 -0500

    Add json assert

--- src/db/firebase/serviceaccount.ts
+++ src/db/firebase/serviceaccount.ts
@@ -1,4 +1,6 @@
-const serviceAccountJSON = (await import(`../../../${process.env.NODE_ENV.toLowerCase()}-serviceaccount.json`)).default
+const serviceAccountJSON = (await import(`../../../${process.env.NODE_ENV.toLowerCase()}-serviceaccount.json`, {
+  assert: { type: 'json' }
+})).default
 const serviceAccount = structuredClone(serviceAccountJSON)
 
 export default serviceAccount

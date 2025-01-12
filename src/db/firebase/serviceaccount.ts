7d70ea35546173f03494f9eac6c4123167355d7b
lunacles <lunahackley@gmail.com>
2025-01-11 17:09:00 -0800

    Delete serviceaccount.ts

--- src/db/firebase/serviceaccount.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Redo where and how this is stored
-// Idk not that important but eh why not
-const serviceAccountJSON = (await import(`../../../${process.env.NODE_ENV.toLowerCase()}-serviceaccount.json`, {
-  assert: { type: 'json' }
-})).default
-const serviceAccount = structuredClone(serviceAccountJSON)
-
-export default serviceAccount
